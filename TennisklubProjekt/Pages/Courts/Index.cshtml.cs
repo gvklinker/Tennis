82143ab01b276c4e87ce7a8b76533176ed5fcd53
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-04-05 10:25:07 +0200

    Skal der skrives noget;

---
+++ TennisklubProjekt/Pages/Courts/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TennisklubProjekt.Pages.Courts
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
